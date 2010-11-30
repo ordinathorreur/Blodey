cec2aac5b35e28a653e72ffca2745266252c2b85
Daniel Todd <todddaniel@gmail.com>
2010-11-30 12:28:22 +0000

    Basic express configuration

--- app.coffee
+++ app.coffee
@@ -1,5 +1,29 @@
-app = (require "express").createServer()
+express = (require "express")
+app = express.createServer()
+
+## CONFIG
+
+# General Config
+app.configure(()->
+	app.use express.methodOverride()
+	app.use express.bodyDecoder()
+	app.use app.router
+	app.use express.staticProvider(__dirname + '/public')
+)
+# Development Environment Config
+app.configure('development', () ->
+    app.use express.errorHandler({ dumpExceptions: true, showStack: true })
+)
+# Production Environmnet Config
+app.configure('development', () ->
+    app.use express.errorHandler()
+)
+
+## ROUTING
+
 app.get('/', (req, res) ->
 	res.send 'hello world'
 )
+
+# Listen to port
 app.listen 3000
--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 (function() {
-  var app;
-  app = (require("express")).createServer();
+  var app, express;
+  express = require("express");
+  app = express.createServer();
+  app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(express.bodyDecoder());
+    app.use(app.router);
+    return app.use(express.staticProvider(__dirname + '/public'));
+  });
+  app.configure('development', function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+  app.configure('development', function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
   app.get('/', function(req, res) {
     return res.send('hello world');
   });
